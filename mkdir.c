f68818429909be2ed436520eb34492cd1ca7f767
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-25 20:30:42 +0530

    Update mkdir.c

--- mkdir.c
+++ mkdir.c
@@ -1,15 +1,13 @@
 // C++ program to create a directory in Linux
-#include <algorithm>
 #include <sys/stat.h>
 #include <sys/types.h>
-using namespace std;
 
 int main(int argc, char **argv)
 
 {
     if (mkdir(argv[1], 0777) == -1)
-        cerr << "Error : " << strerror(errno) << endl;
+        printf("Error");
 
     else
-        cout << "Directory created";
+        printf("Directory created");
 }
