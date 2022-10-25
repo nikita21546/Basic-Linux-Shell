2f708b9474ff8ee4e2ace3cf3d3e56b5ce246ba5
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-25 20:42:54 +0530

    Update rm.c

--- rm.c
+++ rm.c
@@ -19,14 +19,10 @@ int main(int argc, char *argv[])
         fprintf(stderr,"usage: %s path\n",argv[0]);
         exit(1);
     }
-
-    // Delete the directory and its contents by traversing the tree in reverse order, without crossing mount boundaries and symbolic links
-
     if (nftw(argv[1], rmFiles,10, FTW_DEPTH|FTW_MOUNT|FTW_PHYS) < 0)
     {
         perror("ERROR: ntfw");
         exit(1);
     }
-
     return 0;
 }
