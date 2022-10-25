7686961d887a80599bd4f7c0e7b5865f0f07a0b0
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-25 19:07:31 +0530

    Update shell2.c

--- shell2.c
+++ shell2.c
@@ -1,157 +1,67 @@
+//Used for basic input/output stream
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <readline/readline.h>
-#include <readline/history.h>
+//Used for handling directory files
+#include <dirent.h>
+//For EXIT codes and error handling
 #include <errno.h>
-#include <sys/stat.h>
-#define MAX 128
+#include <stdlib.h>
 
-char cmd[MAX], cwd[MAX];
-char temp[MAX][MAX];
-pid_t pid;
-int flag = -1;
-char PATH[MAX] = "/bin/";
-char input_print[MAX];
-int count;
-char buf[MAX];
-
-void set_prompt () {
-    getlogin_r(buf,10);
-    strcpy (input_print, buf);
-    strcat (input_print, ":~");
-    strcat (input_print, getcwd (cwd, sizeof (cwd)));
-}
-
-void handle_input () {
-	printf ("%s\n", input_print);
-	char *input = NULL;
-        input = readline("");
-	if (input == NULL) {
-		printf ("Goodbye\n");
-		exit (1);
+void _ls(const char *dir,int op_a,int op_l)
+{
+	//Here we will list the directory
+	struct dirent *d;
+	DIR *dh = opendir(dir);
+	if (!dh)
+	{
+		if (errno = ENOENT)
+		{
+			//If the directory is not found
+			perror("Directory doesn't exist");
+		}
+		else
+		{
+			//If the directory is not readable then throw error and exit
+			perror("Unable to read directory");
+		}
+		exit(EXIT_FAILURE);
 	}
-	if (strcmp (input, "exit") == 0) {
-		printf ("Goodbye\n");
-		exit (0);
+	//While the next entry is not readable we will print directory files
+	while ((d = readdir(dh)) != NULL)
+	{
+		//If hidden files are found we continue
+		if (!op_a && d->d_name[0] == '.')
+			continue;
+		printf("%s  ", d->d_name);
+		if(op_l) printf("\n");
 	}
-	add_history (input);
-	strcpy (cmd, input);
-	count = 0;
+	if(!op_l)
+	printf("\n");
 }
-
-void change_prompt () {
-    if (strstr (cmd, "PS1") != NULL) {
-        char *t = strtok (cmd, "=");
-        t = strtok (NULL, "=");
-        strcpy (input_print, t);
-        handle_input ();
-    }
-}
-
-void set_path () {
-	char *t = strtok (cmd, "=");
-	t = strtok (NULL, "=");
-	strcpy (PATH, t);
-}
-
-int handle_spaces () {
-	char *t = strtok (cmd, " ");
-	strcpy (temp[0], t);
-	count++;
-	t = strtok (NULL, " ");
-	if (t == NULL) {
-		return 0;
+int main(int argc, const char *argv[])
+{
+	if (argc == 1)
+	{
+		_ls(".",0,0);
 	}
-	int i = 1;
-	while (t != NULL) {
-		strcpy (temp[i], t);
-		i++;
-		count++;
-		t = strtok (NULL, " ");
-	}
-	return -1;
-}
-
-int check_prompt () {
-	char *t = strtok (input_print, "~");
-	t = strtok (NULL, "~");
-	if (strcmp (t, getcwd (cwd, sizeof (cwd))) == 0) {
-		return 1;
+	else if (argc == 2)
+	{
+		if (argv[1][0] == '-')
+		{
+			//Checking if option is passed
+			//Options supporting: a, l
+			int op_a = 0, op_l = 0;
+			char *p = (char*)(argv[1] + 1);
+			while(*p){
+				if(*p == 'a') op_a = 1;
+				else if(*p == 'l') op_l = 1;
+				else{
+					perror("Option not available");
+					exit(EXIT_FAILURE);
+				}
+				p++;
+			}
+			_ls(".",op_a,op_l);
+		}
 	}
 	return 0;
-}
-
-void process_single () {
-	char *ptr[] = {cmd, NULL};
-	if (execv (cmd, ptr) == -1) {
-		char c[MAX];
-		strcpy (c, PATH);
-		strcat (c, cmd);
-		ptr[0] = c;
-		if (execv (c, ptr) == -1) {
-			perror ("error");
-		}
-	}
-}
-
-void process_multiple () {
-	if (strcmp (temp[0], "cd") == 0) {
-		if (check_prompt () == 1) {
-			chdir (temp[1]);
-			set_prompt ();
-		}
-		else {
-			chdir (temp[1]);	
-		}
-		return;
-	}
-	char *ptr[] = {temp[0], NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};	
-	for (int i = 1; i < count; i++) {
-		ptr[i] = temp[i];
-	}
-	if (execv (temp[0], ptr) == -1) {
-		char c[MAX];
-		strcpy (c, PATH);
-		strcat (c, temp[0]);
-		ptr[0] = c;
-		if (execv (c, ptr) == -1) {
-			perror ("error");
-		}
-	}
-}
-
-int main () {
-	set_prompt ();
-	while (1) {
-		handle_input ();
-		if (strstr (cmd, "PATH") != NULL) {
-			set_path ();
-		}	
-		else if (strcmp (cmd, "cd") == 0) {
-			chdir ("/home");
-			set_prompt ();
-		}
-		else if (strstr (cmd, "PS1") != NULL) {
-			change_prompt ();
-			continue;
-		}
-		else {
-			pid = fork ();
-			if (pid == 0) {
-				if (handle_spaces() == 0) {
-					process_single ();
-				}	
-				else {
-					process_multiple ();
-				}
-			}
-			else {
-				wait (NULL);
-			}
-		}
-	}
 }
