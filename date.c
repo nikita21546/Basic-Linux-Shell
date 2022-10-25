2de4a5e8ede13ff9d5bf92d1f025b3a82b1aec92
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-25 19:30:45 +0530

    Update date.c

--- date.c
+++ date.c
@@ -1,9 +1,4 @@
-//Used for basic input/output stream
 #include <stdio.h>
-//Used for handling directory files
-#include <dirent.h>
-//For EXIT codes and error handling
-#include <errno.h>
 #include <stdlib.h>
 #include <time.h>
 
@@ -13,14 +8,11 @@ int main(int argc, const char *argv[])
 	if (argc == 1)
 	{
 		time_t now = time(NULL);
-    
-        if (now == -1) {
-        
-            puts("The time() function failed");
-        }
+   		if (now == -1) {
+			puts("The time() function failed");
+        	}
 
-        printf("%ld\n", now);
-
-        return 0;
+        	printf("%ld\n", now);
+        	return 0;
 	}
 }
