b3d9de362fc93bf3dd81c71c505ecc4a032ae13a
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-25 23:43:28 +0530

    Update date.c

--- date.c
+++ date.c
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
+void _date(const char *dir,int op_a,int op_l, char* b) {
+    
+    char buf[BUF_LEN] = {0};
+    
+    time_t rawtime = time(NULL);
+    
+    if (rawtime == -1) {
+        
+        puts("The time() function failed");
+        return 1;   
+    }
+    
+    struct tm *ptm = localtime(&rawtime);
+    
+    if (ptm == NULL) {
+        
+        puts("The localtime() function failed");
+        return 1;
+    }
+    
+    memset(buf, 0, BUF_LEN);
+    strftime(buf, BUF_LEN, b, ptm);
+    puts(buf);       
+
+    return 0;
+}
 
 int main(int argc, const char *argv[])
 {
@@ -17,5 +43,23 @@ int main(int argc, const char *argv[])
 		}
 		printf("UTC time: %s", asctime(ptm));
 		return 0;
+	}
+	else if (argc == 2)
+	{
+		if (argv[1][0] == '+')
+		{
+			int op_d = 0, op_l = 0;
+			char *p = (char*)(argv[1] + 1);
+			while(*p){
+				if(*p == 'd') op_d = 1;
+				else if(*p == 'l') op_l = 1;
+				else{
+					perror("Option not available");
+					exit(EXIT_FAILURE);
+				}
+				p++;
+			}
+			_date(".",op_a,op_l, argv[2]);
+		}
 	}
 }
