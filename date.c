40633fecd945e7b558747badb40013505a74f30e
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-25 19:28:42 +0530

    Update date.c

--- date.c
+++ date.c
@@ -23,25 +23,4 @@ int main(int argc, const char *argv[])
 
         return 0;
 	}
-	else if (argc == 2)
-	{
-		if (argv[1][0] == '-')
-		{
-			//Checking if option is passed
-			//Options supporting: a, l
-			int op_a = 0, op_l = 0;
-			char *p = (char*)(argv[1] + 1);
-			while(*p){
-				if(*p == 'a') op_a = 1;
-				else if(*p == 'l') op_l = 1;
-				else{
-					perror("Option not available");
-					exit(EXIT_FAILURE);
-				}
-				p++;
-			}
-			_ls(".",op_a,op_l);
-		}
-	}
-	return 0;
 }
