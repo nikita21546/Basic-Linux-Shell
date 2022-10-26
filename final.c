35d761275846996a80f8d1880911d6bdeea03219
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-26 12:24:53 +0530

    Update final.c

--- final.c
+++ final.c
@@ -188,7 +188,12 @@ int main () {
 			    			execl("./rm","./rm", temp[1], NULL);
                     			}	
 					else if(strstr(temp[0],"cat")) {
-						execl("./cat", "./cat", temp[1], temp[2], NULL);
+						if (strstr("", temp[2])){
+							execl("./cat", "./cat", temp[1], NULL);
+						}
+						else{
+							execl("./cat", "./cat", temp[1], temp[2], NULL);
+						}
 					}
 					else {
 						printf("2\n");}
