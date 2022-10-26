23213d71e2e95d30faf1efde7a715a5f35aabe06
nikita21546 <108211279+nikita21546@users.noreply.github.com>
2022-10-26 20:43:50 +0530

    Update rm.c

--- rm.c
+++ rm.c
@@ -14,26 +14,15 @@ static int rmFiles(const char *pathname, const struct stat *sbuf, int type, stru
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2 && argc !=3)
+    if (argc != 2)
     {
         fprintf(stderr,"usage: %s path\n",argv[0]);
         exit(1);
     }
-    if (strcmp(argv[1], "-i")==0){
-        char a;
-        printf("Are you sure you want to remove this file? (y/n)");
-        scanf("%c",&a);
-        if (strcmp(a,"y")==0){
-            if (ftw(argv[2], rmFiles,10) < 0)
-            {
-                perror("ERROR: ntfw");
-                exit(1);
-            }
-        }
-        else{
-            printf("File not removed.\n")
-            return 0;
-        }
+    if (ftw(argv[1], rmFiles,10) < 0)
+    {
+        perror("ERROR: ntfw");
+        exit(1);
     }
     return 0;
 }
